0305a846b1007bc273b847d9474277ceae7037fc
luguaman <148422337+luguaman@users.noreply.github.com>
2024-05-30 18:06:53 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,25 +1,17 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: luguaman <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/15 17:14:20 by luguaman          #+#    #+#             */
-/*   Updated: 2023/11/27 17:57:03 by luguaman         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 100
-#endif
-
-#include <stdio.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
 
 char	*get_next_line(int fd);
-char	*cpy(char *str, char *s, int pos);
-void	open_file(int fd, char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *string, int searchedChar );
+
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t elementCount, size_t elementSize);
+
+size_t	ft_strlen(const char *theString);
+
+#endif
