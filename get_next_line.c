1209f69a112837a1eaa8129b1ffdd413e3331ae8
luguaman <148422337+luguaman@users.noreply.github.com>
2024-05-30 18:05:46 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,58 +1,115 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: luguaman <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/12 10:36:46 by luguaman          #+#    #+#             */
-/*   Updated: 2023/11/28 16:53:31 by luguaman         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+// join and free
+char	*ft_free(char *buffer, char *buf)
 {
-	static int	pos = 0;
-	static char	str[BUFFER_SIZE + 1];
-	char		*s;
-	int			size;
+	char	*temp;
 
-	if (fd == -1)
-		return (NULL);
-	if (pos == 0)
-		open_file(fd, str);
-	size = 0;
-	if (str[pos] != '\0')
-	{
-		while (str[pos + size] != '\n')
-		{
-			size++;
-			if (str[pos + size] == '\0')
-				break ;
-		}
-		s = malloc(size + 1);
-		if (!s)
-			return (NULL);
-		s = cpy(str, s, pos);
-		pos += size + 1;
-		return (s);
-	}
-	return (NULL);
+	temp = ft_strjoin(buffer, buf);
+	free(buffer);
+	return (temp);
 }
 
-int main()
+// delete line find
+char	*ft_next(char *buffer)
 {
-	int		fd;
+	int		i;
+	int		j;
+	char	*line;
+
+	i = 0;
+	// find len of first line
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	// if eol == \0 return NULL
+	if (!buffer[i])
+	{
+		free(buffer);
+		return (NULL);
+	}
+	// len of file - len of firstline + 1
+	line = ft_calloc((ft_strlen(buffer) - i + 1), sizeof(char));
+	i++;
+	j = 0;
+	// line == buffer
+	while (buffer[i])
+		line[j++] = buffer[i++];
+	free(buffer);
+	return (line);
+}
+
+// take line for return
+char	*ft_line(char *buffer)
+{
+	char	*line;
 	int		i;
 
 	i = 0;
-	fd = open("prueba.txt", O_RDONLY);
-	while (i++ < 9)
+	// if no line return NULL
+	if (!buffer[i])
+		return (NULL);
+	// go to the eol
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	// malloc to eol
+	line = ft_calloc(i + 2, sizeof(char));
+	i = 0;
+	// line = buffer
+	while (buffer[i] && buffer[i] != '\n')
 	{
-		printf("%d- %s", i, get_next_line(fd));
+		line[i] = buffer[i];
+		i++;
 	}
-	close(fd);
-	return 0;
+	// if eol is \0 or \n, replace eol by \n
+	if (buffer[i] && buffer[i] == '\n')
+		line[i++] = '\n';
+	return (line);
+}
+
+char	*read_file(int fd, char *res)
+{
+	char	*buffer;
+	int		byte_read;
+
+	// malloc if res dont exist
+	if (!res)
+		res = ft_calloc(1, 1);
+	// malloc buffer
+	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	byte_read = 1;
+	while (byte_read > 0)
+	{
+		// while not eof read
+		byte_read = read(fd, buffer, BUFFER_SIZE);
+		if (byte_read == -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		// 0 to end for leak
+		buffer[byte_read] = 0;
+		// join and free
+		res = ft_free(res, buffer);
+		// quit if \n find
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	free(buffer);
+	return (res);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*line;
+
+	// error handling
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+		return (NULL);
+	buffer = read_file(fd, buffer);
+	if (!buffer)
+		return (NULL);
+	line = ft_line(buffer);
+	buffer = ft_next(buffer);
+	return (line);
 }
